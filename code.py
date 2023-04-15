bb6edead2106108b63a4116a495ab7d300c5aec7
AIsoltani <amirsoltani_os@hotmail.com>
2023-04-15 20:42:20 +0430

    changed something
    Hello
    
    Co-Authored-By: Samane_Sa <samanesayyar7210@gmail.com>

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
-print("hello word")
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage
+import cv2
+
+
+def q52(image, n):
+    profile = skimage.measure.profile_line(image, (n, 0), (n, image.shape[0]))
+    plt.plot(profile)
+    plt.show()
+
+
+image = cv2.imread("cameraman.tif")
+n = int(input('please enter a number:'))
+q52(image, n)
